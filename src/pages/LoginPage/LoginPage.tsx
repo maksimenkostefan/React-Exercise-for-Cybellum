4f918b60b7ecdbcf7b1bb3fec14148f5f1d9d295
maksimenkostefan <s.maksimenko@dunice.net>
2022-03-03 18:02:19 +0300

    Add text

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
+import { Typography } from '@mui/material';
+
 import Form from '../../components/Form';
+import './style.scss';
 
 function LoginPage() {
   return (
-    <Form/>
+    <div id="form-container">
+      <Typography
+        sx={{ mb: 1.5 }}
+        color="textSecondary"
+        align="center"
+      >
+        Log in
+      </Typography>
+      <Typography
+        sx={{ mb: 1.5 }}
+        color="textSecondary"
+        align="center"
+      >
+        Enter your email &#038; Password. If you don't have <br/>
+        an account please see your email invitation
+      </Typography>
+      <Form/>
+    </div>
   );
 }
 
