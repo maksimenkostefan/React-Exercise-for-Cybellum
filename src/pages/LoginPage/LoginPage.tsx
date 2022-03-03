f0203e0fe12b950e8ddaab2583ae21aefae1d96f
maksimenkostefan <s.maksimenko@dunice.net>
2022-03-03 15:57:03 +0300

    Remove custom TextField to use Formik

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 
-import TextField from '../../components/TextField';
-import Button from '../../components/Button';
+import Form from '../../components/Form';
 
 function LoginPage() {
   return (
-    <>
-      <TextField type={'text'} label='Email' />
-      <TextField type={'password'} label='Password' />
-      <Button name='Login' />
-    </>
+    <Form/>
   );
 }
 
