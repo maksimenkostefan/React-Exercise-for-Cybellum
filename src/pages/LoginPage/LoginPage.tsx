a59d465f97b53eeb0a69691f3a26534dbfc6af02
maksimenkostefan <s.maksimenko@dunice.net>
2022-03-03 14:33:43 +0300

    Add login button

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import TextField from '../../components/TextField';
+import Button from '../../components/Button';
 
 function LoginPage() {
   return (
     <>
       <TextField label={'Email'} type={'text'} />
       <TextField label={'Password'} type={'password'} />
+      <Button name='Login' />
     </>
   );
 }
