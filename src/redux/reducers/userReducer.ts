dbf2f3233fd8e0fd26f3014b506a121ce75e5fd5
maksimenkostefan <s.maksimenko@dunice.net>
2022-03-04 17:08:51 +0300

    Add an isLoggedIn attribute

--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -1,34 +1,35 @@
 import {
   LOGIN_REQUESTED, LOGIN_APPROVED, LOGIN_DECLINED,
 } from "../actions/actionTypes";
+import type { IActionType } from '../../types/types';
+import { IUserState } from "../../types/types";
 
-interface userState {
-  loginInfo: {},
-  pending: Boolean,
-}
-
-const initialState: userState = {
+const initialState: IUserState = {
   loginInfo: {},
   pending: false,
+  isLoggedIn: false
 };
 
-const userReducer = (state = initialState, action: { type: string, payload?: {email: string, password: string}}) => {
+const userReducer = (state = initialState, action: IActionType) => {
   switch (action.type) {
     case LOGIN_REQUESTED:
       return {
         ...state,
-        pending: true,
+        pending: true,  
+        isLoggedIn: false
       };
     case LOGIN_APPROVED:
       return {
         ...state,
         pending: false,
         loginInfo: action.payload,
+        isLoggedIn: true
       };
     case LOGIN_DECLINED:
       return {
         ...state,
         pending: false,
+        isLoggedIn: false,
         loginInfo: {},
       };
     default:
