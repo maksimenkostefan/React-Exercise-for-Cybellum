4808e54425f7a7594177adf1d8fcea1a6a0ceb94
maksimenkostefan <s.maksimenko@dunice.net>
2022-03-04 17:08:00 +0300

    Check for authorized credentials in cookies

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-import { Typography } from '@mui/material';
-
-import Form from '../../components/Form';
 import './style.scss';
 
+import Form from '../../components/Form';
+import Text from '../../components/Text/Text';
+import { approveLogin } from '../../redux/actions/actionCreators';
+
+
 function LoginPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const userInfo = document.cookie.split('; ').find(row => row.startsWith('access_token='))?.split('=')[1];
+    if (userInfo) dispatch(approveLogin(userInfo));
+  }, [dispatch]);
+
   return (
-    <div id="form-container">
-      <Typography
-        sx={{ mb: 1.5 }}
-        color="textSecondary"
-        align="center"
-      >
-        Log in
-      </Typography>
-      <Typography
-        sx={{ mb: 1.5 }}
-        color="textSecondary"
-        align="center"
-      >
-        Enter your email &#038; Password. If you don't have <br/>
-        an account please see your email invitation
-      </Typography>
-      <Form/>
-    </div>
+    <>
+      <div id='container'>
+        <div id="form-container">
+          <div id="header-text">
+            <Text 
+              size={26} 
+              weight={500} 
+              color='#53616A'
+            >
+              Log in
+            </Text>
+            <Text 
+              size={14} 
+              weight={500}
+              color='#53616A'
+              opacity='50%'
+            >
+              Enter your email &#038; Password. If you don't have <br/>
+              an account please see your email invitation
+            </Text>
+          </div>
+          <Form/>
+        </div>
+      </div>
+    </>
   );
 }
 
