30bf7d0d175cf7fc18c796bf6ab1f84cc998d2e3
maksimenkostefan <s.maksimenko@dunice.net>
2022-03-04 17:01:35 +0300

    Add Router to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
+import { DashboardRoute } from './pages/Dashboard/DashboardRoute';
+
 import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <LoginPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
+        <Route 
+          path="dashboard" 
+          element={
+            <DashboardRoute component={<Dashboard />} />
+          } 
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
