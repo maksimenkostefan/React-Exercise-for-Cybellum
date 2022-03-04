3ff1f25dd36743a3d49c81278e84e6044af84b8e
maksimenkostefan <s.maksimenko@dunice.net>
2022-03-04 17:03:15 +0300

    Change email to username in login credentials

--- src/redux/actions/actionCreators.ts
+++ src/redux/actions/actionCreators.ts
@@ -2,6 +2,6 @@ import {
   LOGIN_REQUESTED, LOGIN_APPROVED, LOGIN_DECLINED,
 } from './actionTypes';
 
-export const requestLogin = (payload: {email: string, password : string}) => ({ type: LOGIN_REQUESTED, payload });
+export const requestLogin = (payload: {username: string, password : string}) => ({ type: LOGIN_REQUESTED, payload });
 export const approveLogin = (payload: {}) => ({ type: LOGIN_APPROVED, payload });
 export const declineLogin = (error: {}) => ({ type: LOGIN_DECLINED, error });
