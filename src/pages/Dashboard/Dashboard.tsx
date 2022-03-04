a0ebcc508900541a1caf2f9ceb5274dacacc9972
maksimenkostefan <s.maksimenko@dunice.net>
2022-03-04 17:03:45 +0300

    Create a basic Dashboard page

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <>
+      You have logged in and are now on the dashboard
+    </>
+  );
+}
+
+export default React.memo(Dashboard);
